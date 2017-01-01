59f53e5c440b3b8d326aee1ece980b53b59fa32b
Wutthinan Wanchongkham <wutthinan@Wutthinans-MacBook-Air.local>
2017-01-01 22:02:51 +0700

    get claim from request

--- src/main/java/com/foamtec/ApiController.java
+++ src/main/java/com/foamtec/ApiController.java
@@ -1,8 +1,11 @@
 package com.foamtec;
 
+import io.jsonwebtoken.Claims;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * Created by wutthinan on 1/1/2017 AD.
@@ -12,7 +15,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class ApiController {
 
     @RequestMapping(value = "/role", method = RequestMethod.GET, headers = "Content-Type=Application/json")
-    public String api() {
+    public String api(HttpServletRequest request) {
+        Claims claims = (Claims) request.getAttribute("claims");
+        System.out.println("Claims ==> "+claims);
         return "api";
     }
 }
